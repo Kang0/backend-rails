607582231e4510dc3a89306eabd0132572ff12bd
Kang0 <kevinkangg@gmail.com>
2019-06-10 23:58:07 -0400

    update crud action in challanges_controller

--- app/controllers/api/v1/challenges_controller.rb
+++ app/controllers/api/v1/challenges_controller.rb
@@ -1,24 +1,27 @@
 class Api::V1::ChallengesController < Api::V1::BaseController
 
     def index
-        @challenges = Challenge.all
-        
-        render :json => @challenges
+        respond_with Challenge.all
     end
 
     def create
+        respond_with :api, :v1, Challenge.create(challenge_params)
     end
 
     def destroy
+        respond_with Item.destroy(params[:id])
     end
 
     def update
+        item = Item.find(params[:id])
+        item.update_attribute(items_params)
+        respond_with item, json: item
     end
 
     private
 
     def challenge_params
-        params.require(:challanges).permit(:name, :daysLeft)
+        params.require(:challenges).permit(:name, :daysLeft)
     end
 
 end
