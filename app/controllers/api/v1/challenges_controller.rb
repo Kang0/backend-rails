9c5b896496cb36769f0c57c76b6c72388fd8200d
Kang0 <kevinkangg@gmail.com>
2019-06-10 23:55:53 -0400

    add crud to challenges_controller

--- app/controllers/api/v1/challenges_controller.rb
+++ app/controllers/api/v1/challenges_controller.rb
@@ -1,9 +1,24 @@
-class V1::ChallengesController < ApplicationController
+class Api::V1::ChallengesController < Api::V1::BaseController
 
     def index
         @challenges = Challenge.all
         
         render :json => @challenges
+    end
+
+    def create
+    end
+
+    def destroy
+    end
+
+    def update
+    end
+
+    private
+
+    def challenge_params
+        params.require(:challanges).permit(:name, :daysLeft)
     end
 
 end
--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,5 +1,3 @@
 class Challenge < ApplicationRecord
 
-    
-
 end
