adcb23fa870bcf32928bf8565d3ded77e31958c2
Kang0 <kevinkangg@gmail.com>
2019-06-11 22:43:18 -0400

    add JWT encode and decode methods / update user login controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,35 @@
 class ApplicationController < ActionController::API
     include ActionController::MimeResponds
+    include ActionController::HttpAuthentication::Token::ControllerMethods #don't really know what this does
+
+    before_action :authenticate 
+    #this will run before any controller action
+    #can't accidently forget to add authentication for controller actions
+
+
+    private
+
+    def current_user
+
+    end
+
+    def authenticate
+
+    end
+
+    #JWT METHODS 
+
+    def secret_key
+        'secret'
+    end
+    #not the best security to just have a random string, be better to receive an random string
+
+    def encode(payload)
+        JWT.encode(payload, secret_key, 'HS256')
+    end
+
+    def decode(token)
+        JWT.decode(token, secret_key, true, { algorithm: 'HS256' })
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_11_020412) do
+ActiveRecord::Schema.define(version: 2019_06_12_021647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,17 @@ ActiveRecord::Schema.define(version: 2019_06_11_020412) do
   create_table "challenges", force: :cascade do |t|
     t.string "name"
     t.integer "daysLeft"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_challenges_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "challenges", "users"
 end
