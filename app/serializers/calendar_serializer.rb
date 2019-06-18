8c66826e5b3b4a78e40acf8e2803127dff16456a
Kang0 <kevinkangg@gmail.com>
2019-06-18 15:23:11 -0400

    fix calendar order in challenge serializer

--- app/serializers/calendar_serializer.rb
+++ app/serializers/calendar_serializer.rb
@@ -1,5 +1,5 @@
 class CalendarSerializer < ActiveModel::Serializer
-  attributes :id, :years, :months, :dates, :challenge_id, :clicked
+  attributes :id, :years, :months, :date, :challenge_id, :clicked
 
   belongs_to :challenge
 end
